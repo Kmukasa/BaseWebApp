b18f0e68da2afaf4fe5aa9115e3f4a62d96397c9
Keisha Mukasa <keishamukasa@Keishas-MacBook-Pro.local>
2019-03-30 14:52:54 -0400

    added Database- firebase

--- project/js/main.js
+++ project/js/main.js
@@ -9,3 +9,44 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+// Gets called whenever the user clicks "sign in" or "sign out".
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+  //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+  $('#login-button').attr("disabled", true);
+}
+
+function handleMessageFormSubmit(){
+
+  var FirstName = $("#FirstName").val();
+  var LastName = $("#LastName").val();
+  var Age = $("#Age").val();
+  var FaveSuperhero = $("#SuperHero").val();
+  var body = FirstName + " " + LastName + " " + Age + " " + FaveSuperhero;
+  console.log(body);
+  addMessage(FirstName, LastName, Age, FaveSuperhero);
+}
+
+function addMessage(var1, var2, var3, var4){
+
+  var postData = {
+    FirstName: var1,
+    LastName: var2,
+    Age: var3,
+    FaveSuperhero: var4
+  }
+  
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+
+}
